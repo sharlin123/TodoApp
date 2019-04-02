b128de211d2bba29fb1104b8d0b2e092c9ec6e1d
sharlin123 <sharlin.skr@gmail.com>
2019-04-03 00:22:11 +0530

    Express based NodeJS App

---
+++ models/todo.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+const Schema= mongoose.Schema;
+let todoSchema=new Schema({
+	description:{
+		type:String,
+		required:true
+	},
+	done:{
+		type:Boolean,
+		default:false
+	}
+});
+module.exports=mongoose.model('Todo',todoSchema);
